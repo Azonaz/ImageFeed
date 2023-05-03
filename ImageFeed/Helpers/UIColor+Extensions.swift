3e9172fec1c1dccfe171e4ac525374e3516bebc0
Azonaz <malinina.a@gmail.com>
2023-05-03 16:23:54 +0300

    Закончена задача по 8 спринту: - создан новый проект, - сверстан Launch Screen, - сверстан главный экран с заданными параметрами.

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+extension UIColor {
+    static let ypBlack = UIColor (named: "ypBlack")
+    static let YPGradient1  = UIColor(named: "ypLinearGradient1")
+    static let YPGradient2 = UIColor(named: "ypLinearGradient2")
+    
+}
---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static var reuseIdentifier = "ImagesListCell"
+    var gradientLayer: CAGradientLayer!
+    private let gradientHeight = 30.0
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        gradientLayer = CAGradientLayer()
+        if let gradient1 = UIColor.YPGradient1?.cgColor, let gradient2 = UIColor.YPGradient2?.cgColor {
+            gradientLayer.colors = [gradient1, gradient2]
+        }
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = CGRect(x: 0, y: cellImage.bounds.height - gradientHeight, width: cellImage.bounds.width, height: gradientHeight)
+        cellImage.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = CGRect(x: 0, y: cellImage.bounds.height - 30, width: cellImage.bounds.width, height: 30)
+    }
+}
+
